ef250aae5539b920323b00189d1184d1f1594a6a
Sagnik Sarkar <sagnik106@gmail.com>
2021-01-17 05:48:59 +0530

    Responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,21 +8,21 @@ class Contact extends React.Component{
                 <div class='cdivs'>
                     <table>
                         <tr>
-                            <td style={{textAlign: 'right'}} colspan='2' id='github'><a href="https://github.com/sagnik106" target="_blank" rel="noreferrer"><img width="30px" height="30px" alt="github" src="github.png"></img></a></td>
-                            <td style={{textAlign: 'center'}}>Destination for cool projects</td>
+                            <td style={{textAlign: 'center'}}><a href="https://github.com/sagnik106" target="_blank" rel="noreferrer"><img class='hoverer' alt="github" src="github.png"></img></a></td>
+                            <td style={{textAlign: 'center'}} colspan='2'>Destination for cool projects</td>
                         </tr>
                         <tr>
-                            <td style={{textAlign: 'center'}}>Professional life</td>
-                            <td style={{textAlign: 'left'}} colspan='2' id='linkedin'><a href="https://linkedin.com/lesagniksarkar" target="_blank" rel="noreferrer"><img width="30px" height="30px" alt="linkedin" src="linkedin.png"></img></a></td>
+                            <td style={{textAlign: 'center'}} colspan='2'>Professional life</td>
+                            <td style={{textAlign: 'center'}}><a href="https://linkedin.com/in/lesagniksarkar" target="_blank" rel="noreferrer"><img class='hoverer' alt="linkedin" src="linkedin.png"></img></a></td>
                         </tr>
                         <tr>
-                            <td style={{textAlign: 'right'}} colspan='2' id='twitter'><a href="https://twitter.com/lesagniksarkar" target="_blank" rel="noreferrer"><img width="30px" height="30px" alt="twitter" src="twitter.png"></img></a></td>
-                            <td style={{textAlign: 'center'}}>Straight out of my head</td>
+                            <td style={{textAlign: 'center'}}><a href="https://twitter.com/lesagniksarkar" target="_blank" rel="noreferrer"><img class='hoverer' alt="twitter" src="twitter.png"></img></a></td>
+                            <td style={{textAlign: 'center'}} colspan='2'>Straight out of my head</td>
                         </tr>
                     </table>
                 </div>
                 <div class='cdivs'>
-                    <table style={{margin: '10vh auto'}}>
+                    <table class="ctable">
                         <tr>
                             <td class='td1'>e-Mail</td>
                             <td class='td1'>:</td>
